038c006a5778e4fa51b3e93a7d97e60c16cc43f9
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-04-30 14:39:36 +0300

    Yazarlar_Edit_Details _yapildi
    Bitmedi daha proje

---
+++ DersteYapılanlar/Sales/Sales/Models/Concrete/CategoryDAL.cs
@@ -0,0 +1,12 @@
+﻿using Sales.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales.Models.Concrete
+{
+    public class CategoryDAL:BaseRepository<Category>
+    {
+    }
+}
---
+++ Ödevler/Sales/Sales/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.Models.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sales.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly SalesDbContext _context;
+
+        public CategoryController(SalesDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var categories = new CategoryDAL();
+            return View(categories.GetAll());
+        }
+    }
+}
