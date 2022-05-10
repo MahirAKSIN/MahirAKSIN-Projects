f371ff07c7577744183fff5bd93d38b9018560ec
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-05-10 22:04:54 +0300

    New Project(Blog)

---
+++ KendiCalişmalarım/CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+
+        DbSet<About> Abouts { get; set; }
+        DbSet<Blog> Blogs{ get; set; }
+        DbSet<Category > Categories{ get; set; }
+        DbSet<Comment> Comments{ get; set; }
+        DbSet<Contact>Contacts{ get; set; }
+        DbSet<Writer>Writers{ get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(LocalDB)\\MSSQLLocalDB;database=CoreDbBlog;integrated security=true;");
+        }
+
+
+
+    }
+}
