a1b1b78abb963af9cf13c3ccc9736cd5ef225b55
MahirAKSIN <98876295+MahirAKSIN@users.noreply.github.com>
2022-05-10 15:05:50 +0300

    Eklenti

---
+++ DersteYapılanlar/MiniShopApp/MiniShopApp.Business/Abstract/IProductService.cs
@@ -0,0 +1,27 @@
+﻿using MiniShopApp.Entityy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.Business.Abstract
+{
+    public interface IProductService
+    {
+
+
+        Product GEtById(int id);
+        List<Product> GetAll();
+        void Update(Product entity);
+        void Create(Product entity);
+        void Delete(Product entity);
+
+
+
+
+
+
+
+    }
+}
