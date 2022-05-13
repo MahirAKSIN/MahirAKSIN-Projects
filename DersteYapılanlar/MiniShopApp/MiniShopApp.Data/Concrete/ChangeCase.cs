23190484b4020722beb482fffd0b0ed51cd23e40
MahirAKSIN <98876295+MahirAKSIN@users.noreply.github.com>
2022-05-13 13:04:35 +0300

    Alertmessage add

---
+++ DersteYapılanlar/MiniShopApp/MiniShopApp.Data/Concrete/ChangeCase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShopApp.Data.Concrete
+{
+    public class ChangeCase
+    {
+        public string ConvertString(string text)
+        {
+
+
+            text = text.Replace("I", "i");
+            text = text.Replace("İ", "i");
+            text = text.Replace("ı", "i");
+            text = text.ToLower();
+
+            text = text.Replace("ç", "c");
+            text = text.Replace("ö", "o");
+            text = text.Replace("ş", "s");
+            text = text.Replace("ğ", "g");
+            text = text.Replace("ü", "u");
+
+
+
+            return text;
+        }
+
+    }
+}
--- DersteYapılanlar/MiniShopApp/MiniShopApp.Data/Concrete/EfCore/EfCoreProductRepository.cs
+++ DersteYapılanlar/MiniShopApp/MiniShopApp.Data/Concrete/EfCore/EfCoreProductRepository.cs
@@ -11,6 +11,28 @@ namespace MiniShopApp.Data.Concrete.EfCore
 {
     public class EfCoreProductRepository : EfCoreGenericRepository<Product, MiniShopContext>, IProductRepository
     {
+
+        private string ConvertString(string text)
+        {
+
+
+            text = text.Replace("I", "i");
+            text = text.Replace("İ", "i");
+            text = text.Replace("ı", "i");
+            text = text.ToLower();
+
+            text = text.Replace("ç", "c");
+            text = text.Replace("ö", "o");
+            text = text.Replace("ş", "s");
+            text = text.Replace("ğ", "g");
+            text = text.Replace("ü", "u");
+
+
+
+            return text;
+        }
+
+
         public List<Product> GetHomePageProducts()
         {
             using (var context = new MiniShopContext())
@@ -59,12 +81,13 @@ namespace MiniShopApp.Data.Concrete.EfCore
 
         public List<Product> GetSearchResult(string searchString)
         {
-            searchString = searchString.ToLower();
+            ChangeCase s = new ChangeCase();
+            searchString = ConvertString(searchString);
             using (var context= new MiniShopContext())
             {
                 var products = context
                     .Products
-                    .Where(i => i.IsApproved && (i.Name.ToLower().Contains(searchString) || i.Description.ToLower().Contains(searchString)))
+                    .Where(i => i.IsApproved && (ConvertString(i.Name).Contains(searchString) || ConvertString(i.Description.ToLower()).Contains(searchString)))
                     .ToList();
                 return products;
             }
