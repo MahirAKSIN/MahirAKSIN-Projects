a6f33f114bf155ecf6310cf0c247d46f77b80836
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-05-17 01:00:58 +0300

    ViewComponent

--- KendiCalişmalarım/CoreDemo/BusinessLayer/Concrete/CategoryManager.cs
+++ KendiCalişmalarım/CoreDemo/BusinessLayer/Concrete/CategoryManager.cs
@@ -20,6 +20,8 @@ namespace BusinessLayer.Concrete
          * Avantaj olaraksa kısa vadede daha az kod yazıp daha kısa sürede projeyi bitirebiliriz.*/
 
         //GenericRepository<Category> genericRepository = new GenericRepository<Category>();
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        
 
         ICategoryDAL _cd;
 
